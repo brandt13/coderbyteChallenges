a437264e7cb47a4e0de58741f7d54ca1f8608694
brandt13 <brandtleeds@gmail.com>
2018-07-30 17:55:55 -0500

    first commit / Find Longest Word

---
+++ src/main/java/challenges/FindLongestWord.java
@@ -0,0 +1,36 @@
+package challenges;
+
+import java.util.*;
+import java.io.*;
+
+public class FindLongestWord {
+
+    /*
+       This challenge asks you to find the longest word out of a sentence, excluding anything but
+       letters and numbers (which are allowed to be added to words for some reason). In this challenge,
+       I learned more about regex and how to split Strings exactly where you want them to be split.
+    */
+
+    public static String LongestWord(String sentence) {
+
+        String[] words = sentence.split("[^a-zA-Z0-9]");
+        String longestWord = "";
+
+        for (int i = 0; i < words.length; i++){
+
+            if (words[i].length() > longestWord.length()){
+
+                longestWord = words[i];
+            }
+        }
+
+        return longestWord;
+
+    }
+
+    public static void main (String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print(LongestWord(s.nextLine()));
+    }
+}
---
+++ src/main/java/readMe.txt
@@ -0,0 +1,9 @@
+
+Hello! Brandt here.
+
+These are some coding challenges from coderbyte.com I'm doing to get into the habit of coding every day. I will
+start with Java and not worry about integrating these with anything like Maven, Spring, Tomcat or AWS (unless the
+challenges call for it). These challenges are for me to get a deeper understanding of the intricacies of Java.
+
+- b
+
